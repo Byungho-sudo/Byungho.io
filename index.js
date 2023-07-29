c8f8bcc549ba67db9960b30c5e2e066f14430e9b
Byungho-sudo <ariosha42@gmail.com>
2023-07-29 14:48:29 +0200

    making temporary correction to design

--- index.js
+++
@@ -1,9 +0,0 @@
-let data = ['Apple V-1', 'Can V-1'];
- 
-let list = document.getElementById("ItemList");
- 
-data.forEach((item)=>{
-  let li = document.createElement("li");
-  li.innerText = item;
-  list.appendChild(li);
-})
