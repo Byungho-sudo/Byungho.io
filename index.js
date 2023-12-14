bc98b1314ebd32ad80ff2d2f27e85360cffba201
Byungho-sudo <ariosha42@gmail.com>
2023-12-14 05:13:45 +0100

    JS Comments

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ imgList.forEach((element) => {
   console.log(element);
   element.classList.add("card");
 });
+
+
+// OBS!!! Section "GearTestList" is linked to "Test Site" in "Sorting_Test.html"
+// const GearTestList = document.getElementsByName("Gear");
+// GearTestList.sort((a, b) => a.id - b.id);
+// console.log(GearTestList)
+// Trying to sort Gears by id or date of upload, goal is to have a sort by [GEAR NAME] and have the same [NAME] sort by [ID / UPLOAD DATE] where oldest comes last and newest first and hide other [NAME]'s
