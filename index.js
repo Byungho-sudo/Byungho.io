59994e5e135ba9b831458b634b89a203db654801
Byungho-sudo <ariosha42@gmail.com>
2024-01-19 16:57:10 +0100

    *CSS: changing sizes and improving img padding,
    *JS temp: removing about page

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function loadNavigation() {
   document.write(`
     <div class="topnav">
       <a class="material-icons" href="../../index.html">home</a>
-      <a href="About.html">About</a>
+
     </div>`
   );
 }
-
+/* <a href="About.html">About</a> */
