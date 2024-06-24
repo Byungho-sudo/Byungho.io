a6a20c46c3a3f79f7ee69b2c989596e52369a168
Byungho-sudo <ariosha42@gmail.com>
2024-06-24 14:21:45 +0200

    NavBar hotfix 1

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function loadNavigation() {
   document.write(`
     <div class="topnav">
       <a class="material-icons" href="../../index.html">home</a>
-
-    </div>`
-  );
+    </div>
+  `);
 }
 /* <a href="About.html">About</a> */
